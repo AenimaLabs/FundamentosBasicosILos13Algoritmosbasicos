29564268aebc22d841c6102dd05cb3b3280c1f52
JorgeAndaur <aenimalabs@gmail.com>
2021-08-25 01:53:39 -0400

    parte del algoritmo

---
+++ algoritmos.js
@@ -0,0 +1,44 @@
+    
+   
+/*arreglo del 1 al 255
+ arreglo=[]
+
+    for (var i=1; i<256; i++) {
+             
+          contador=i;
+          arreglo.push(contador);
+        
+        }console.log(arreglo);*/  
+      
+        
+        
+        
+
+
+    /*Consigue pares hasta 1000
+    for (var i=2; i<1001; i++) {
+        if ((i % 2) == 0)  {
+            console.log(i)
+        }
+    }*/
+
+
+    /*sumatoria impares
+    function sumaimpares (x){        
+        b=0;
+    for (var i=0; i<=x; i++) {
+        if ((i % 2) != 0)  {
+            b +=i;
+            
+        }
+    } return b}
+    console.log(sumaimpares(5000));*/
+
+    /* 4 itera un array
+
+
+
+*/
+
+    /* 5 
+*/
